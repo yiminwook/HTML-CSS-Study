f95ac9c169d961078f46151eb0fa76f9f03595ac
yiminwook <grs0416@gmail.com>
2023-02-26 21:02:52 +0900

    Fix: Type guard

--- basic01/index.ts
+++ basic01/index.ts
@@ -1,46 +1,73 @@
 const selectPlanButtons = document.querySelectorAll(
   ".plan button"
 ) as NodeListOf<HTMLButtonElement>;
-const backdrop = document.querySelector(".backdrop")! as HTMLDivElement;
-const modal = document.querySelector(".modal")! as HTMLDivElement;
+const backdrop = document.querySelector(".backdrop") as HTMLDivElement;
+const modal = document.querySelector(".modal") as HTMLDivElement;
 const modalButton = (
   document.getElementsByClassName(
     "modal__action"
   ) as HTMLCollectionOf<HTMLAnchorElement>
-).item(0)!;
+).item(0);
 const modalButtonNegative = (
   document.getElementsByClassName(
     "modal__action--negative"
   ) as HTMLCollectionOf<HTMLButtonElement>
-).item(0)!;
+).item(0);
 
 const mobileNavButton = document.querySelector(
   ".toggle-button"
-)! as HTMLButtonElement;
+) as HTMLButtonElement;
 
 const mobileNav = document.querySelector(".mobile-nav") as HTMLElement;
 
 selectPlanButtons.forEach((selectPlanButton) => {
   selectPlanButton.addEventListener("click", () => {
     // 인라인 스타일이 기존 css를 덮어씌운다.
-    modal.style.display = "block";
-    backdrop.style.display = "block";
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    if (modal) {
+      modal.classList.add("open");
+    }
+    if (backdrop) {
+      backdrop.classList.add("open");
+    }
   });
 });
 
 function closeModal() {
-  modal.style.display = "none";
-  backdrop.style.display = "none";
+  // modal.style.display = "none";
+  // backdrop.style.display = "none";
+  if (modal) {
+    modal.classList.remove("open");
+  }
+  if (backdrop) {
+    backdrop.classList.remove("open");
+  }
 }
 
-backdrop.addEventListener("click", () => {
-  mobileNav.style.display = "none";
-  closeModal();
-});
+if (backdrop) {
+  backdrop.addEventListener("click", () => {
+    // mobileNav.style.display = "none";
+    if (mobileNav) {
+      mobileNav.classList.remove("open");
+    }
+    closeModal();
+  });
+}
 
-modalButtonNegative.addEventListener("click", closeModal);
+if (modalButtonNegative) {
+  modalButtonNegative.addEventListener("click", closeModal);
+}
 
-mobileNavButton.addEventListener("click", () => {
-  mobileNav.style.display = "block";
-  backdrop.style.display = "block";
-});
+if (mobileNavButton) {
+  mobileNavButton.addEventListener("click", () => {
+    // mobileNav.style.display = "block";
+    // backdrop.style.display = "block";
+    if (mobileNav) {
+      mobileNav.classList.add("open");
+    }
+    if (backdrop) {
+      backdrop.classList.add("open");
+    }
+  });
+}

ced7084e417e09d34b6072c52017b72347dfd006
yiminwook <grs0416@gmail.com>
2023-03-02 22:26:51 +0900

    Feat: animation and transform

--- basic01/index.ts
+++ basic01/index.ts
@@ -19,37 +19,42 @@ const mobileNavButton = document.querySelector(
 ) as HTMLButtonElement;
 
 const mobileNav = document.querySelector(".mobile-nav") as HTMLElement;
+const ctaButton = document.querySelector(
+  ".main-nav__item--cta"
+) as HTMLOListElement;
 
 selectPlanButtons.forEach((selectPlanButton) => {
   selectPlanButton.addEventListener("click", () => {
     // 인라인 스타일이 기존 css를 덮어씌운다.
-    // modal.style.display = "block";
-    // backdrop.style.display = "block";
     if (modal) {
-      modal.classList.add("open");
+      modal.style.display = "block";
+      //순차적으로 css를 적용시킨다.
+      setTimeout(() => modal.classList.add("open"), 10);
     }
     if (backdrop) {
-      backdrop.classList.add("open");
+      backdrop.style.display = "block";
+      setTimeout(() => backdrop.classList.add("open"), 10);
     }
   });
 });
 
 function closeModal() {
-  // modal.style.display = "none";
-  // backdrop.style.display = "none";
   if (modal) {
     modal.classList.remove("open");
+    // display=none이 transtion이 작동후에 적용될 수 있도록 한다.
+    setTimeout(() => (modal.style.display = "none"), 500);
   }
   if (backdrop) {
     backdrop.classList.remove("open");
+    setTimeout(() => (backdrop.style.display = "none"), 500);
   }
 }
 
 if (backdrop) {
   backdrop.addEventListener("click", () => {
-    // mobileNav.style.display = "none";
     if (mobileNav) {
       mobileNav.classList.remove("open");
+      setTimeout(() => (mobileNav.style.display = "none"), 500);
     }
     closeModal();
   });
@@ -61,13 +66,13 @@ if (modalButtonNegative) {
 
 if (mobileNavButton) {
   mobileNavButton.addEventListener("click", () => {
-    // mobileNav.style.display = "block";
-    // backdrop.style.display = "block";
     if (mobileNav) {
-      mobileNav.classList.add("open");
+      mobileNav.style.display = "block";
+      setTimeout(() => mobileNav.classList.add("open"), 10);
     }
     if (backdrop) {
-      backdrop.classList.add("open");
+      backdrop.style.display = "block";
+      setTimeout(() => backdrop.classList.add("open"), 10);
     }
   });
 }
@@ -75,4 +80,19 @@ if (mobileNavButton) {
 const form = document.querySelector(".signup-form") as HTMLFormElement;
 if (form) {
   form.addEventListener("click", (e) => {});
+}
+
+if (ctaButton) {
+  ctaButton.addEventListener("animationstart", (e) => {
+    // console.log(e);
+    // console.log("start");
+  });
+  ctaButton.addEventListener("animationend", (e) => {
+    // console.log(e);
+    // console.log("end");
+  });
+  ctaButton.addEventListener("animationiteration", (e) => {
+    // console.log(e);
+    // console.log("literation");
+  });
 }
